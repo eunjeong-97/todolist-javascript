d663ff912f8d5685c5eab9fd4c7924c28546a593
박은정 <beanlove97@gmail.com>
2022-01-14 12:56:55 +0900

    1일차: Login 로직구현 - 이름 입력하면 input 없애고 인사말표시 - localStorage 삭제하는 버튼생성

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # todolist-javascript
-JavaScript를 통해 To Do List를 만듭니다.
+
+> 2022.01.14 ~ 진행중
+
+- JavaScript를 통해 To Do List를 만듭니다.
+- [notion Page](https://fish-twill-d55.notion.site/a742c37365474ce8a5b5ff7d0c8e44c6)
+- [Github Page]()
+
+
+
---
+++ javascript/login.js
@@ -0,0 +1,37 @@
+const USERNAME_KEY = 'username';
+const HIDDEN_CLASSNAME = 'hidden';
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
+const saved_username = localStorage.getItem(USERNAME_KEY);
+const removeButton = document.querySelector('button');
+
+loginForm.addEventListener('submit', handleLoginSubmit);
+removeButton.addEventListener('click', removeLocalstorage);
+
+function handleLoginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, username);
+  writeGreetings(username);
+}
+
+function writeGreetings(username) {
+  const GREETING_CONTENTS = `Hello, ${username}`;
+  greeting.innerText = GREETING_CONTENTS;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function removeLocalstorage() {
+  localStorage.removeItem(USERNAME_KEY);
+  location.reload();
+}
+
+if (saved_username === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  localStorage.removeItem(USERNAME_KEY);
+} else {
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  writeGreetings(saved_username);
+}
